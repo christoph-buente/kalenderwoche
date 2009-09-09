927c301c230e6cf8df8eae5e6b2a53b2456b09c6
Christoph Bünte <info@christophbuente.de>
2009-09-09 20:23:11 +0200

    added root route

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,17 +1,12 @@
 class HomesController < ApplicationController
   include HomesHelper
-  caches_action :index
+  # caches_action :index
   
   def index
     start_date = 1.year.ago
     end_date = 2.years.from_now
     years = (start_date.year..end_date.year)
-    respond_to do |format|
-      format.ics do
-        headers['Content-Type'] = "text/plain; charset=UTF-8"
-        # headers['Content-Type'] = "text/calendar; charset=UTF-8"
-        render :text => calendar(years), :layout => false
-      end
-    end
+    headers['Content-Type'] = "text/calendar; charset=UTF-8"
+    render :text => calendar(years), :layout => false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   
+  map.root      :controller => 'homes', :action => 'index', :format => :ics
   map.resources :homes, :as => 'kalenderwochen', :only => [:index, :show]
   
   map.connect ':controller/:action/:id'
