db3b4b0b2625030d691df378577d601d871f436b
Christoph Bünte <info@christophbuente.de>
2009-09-18 21:21:17 +0200

    fixed calculation of calendar week

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,9 +3,9 @@ class HomesController < ApplicationController
   # caches_action :index
   
   def index
-    start_date = 1.year.ago
-    end_date = 2.years.from_now
-    years = (start_date.year..end_date.year)
+    start_date = 1.years.ago
+    end_date = 1.years.from_now
+    years = (start_date.year..end_date.year).to_a
     headers['Content-Type'] = "text/calendar; charset=UTF-8"
     render :text => calendar(years), :layout => false
   end
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,5 +1,6 @@
 module HomesHelper
   require 'icalendar'
+  require 'digest/sha1'
   
   def calendar(years)
     cal = Icalendar::Calendar.new
@@ -7,36 +8,36 @@ module HomesHelper
     cal.custom_property("X-WR-CALNAME", "Kalenderwochen")
     cal.custom_property("X-WR-CALDESC", "Kalenderwochen beginnend mit Montag")
     
-    years.each_with_index do |year,i|
-      first_week = start_of_first_week(year)
-      52.times do |week|
-        cal.add_event(event_for_week(year,week,i))
+    count = 0
+    years.each do |year|
+      monday = start_of_first_week(year)
+      last_day = end_of_last_week(year)
+      while monday <= last_day do
+        cal.add_event(event_for_week(monday,count))
+        monday += 6
+        count += 1
       end
     end
     cal.to_ical
   end
   
-  def event_for_week(year, week, index)
+  def event_for_week(monday, count)
     event              = Icalendar::Event.new
-    event.start        = start_of_week(year, week)
-    event.end          = end_of_week(year, week)
-    event.summary      = "KW #{week+1}"
+    event.start        = monday
+    event.end          = monday + 6
+    event.summary      = "KW #{monday.cweek}"
     event.klass        = "PUBLIC"
-    event.sequence     = (index * 52) + week
-    event.uid          = "KW #{index}"
+    event.sequence     = count
+    event.uid          = Digest::SHA1.hexdigest(monday.to_s)
     event
   end
-  
-  def start_of_week(year, week)
-    start_of_first_week(year) + (week * 7).days
-  end
-  
-  def end_of_week(year, week)
-    start_of_week(year, week) +  7.days
-  end
-  
+    
   def start_of_first_week(year)
     fourth_of_january = Date.parse("04.Jan.#{year}")
     fourth_of_january.beginning_of_week
+  end
+  
+  def end_of_last_week(year)
+    Date.parse("04.Jan.#{year}").end_of_year
   end
 end
