998126ed33ceba17550b02d77916c9330c280c04
Christoph Bünte <info@christophbuente.de>
2009-09-18 20:02:38 +0200

    doign a fix

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -36,7 +36,7 @@ module HomesHelper
   end
   
   def start_of_first_week(year)
-    fourth_of_january = Date.parse("04.01.#{year}")
+    fourth_of_january = Date.parse("04.Jan.#{year}")
     fourth_of_january.beginning_of_week
   end
 end
